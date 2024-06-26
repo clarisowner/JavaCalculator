4f67802676c6bc53dbbc208a8cae4f3da7575b44
Maksim Shesternev <shesternev.m@gmail.com>
2024-06-26 21:11:12 +0300

    new commit

--- JavaCalculator.java
+++ JavaCalculator.java
@@ -1,10 +1,22 @@
-import java.util.Scanner; //// все классы пакета util надо импортировать вручню, теперь можем получать данные на ввод
+import java.util.Scanner; // все классы пакета util надо импортировать вручню, теперь можем получать данные на ввод
 public class JavaCalculator {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in); // инициализируем объект Scanner, который считывает данные из ввода в консоль
         System.out.println("                 ***JavaCalculator***                   ");
-        System.out.println("введите свое выражение, которое надо посчитать (например 1+2):"); //по запуску программы отобразиться сперва этот текст
-        String input = scn.nextLine()
+        System.out.println("введите свое выражение, которое надо посчитать (например 1+2):"); // по запуску программы отобразиться сперва этот текст
+        String inp = scn.nextLine(); // метод считывает введеные данные в консоль
+
+        try {
+            String res = calc(inp);
+            System.out.println("результат: " + res);
+        }catch (Exepiton e) {
+                System.out.println("ошибка: " + e.getMessage());
+            }
+
+
+
+
+        }
 
 
 
--- out/production/JavaCalculator/README.md
+++ out/production/JavaCalculator/README.md
@@ -1,6 +1,6 @@
 JavaCalculator
 
-My name is Maxim Shesternem, and I present my first project — a simple calculator written in Java. 
+My name is Maxim Shesternev, and I present my first project — a simple calculator written in Java. 
 This calculator supports basic arithmetic operations (addition, subtraction, multiplication and division)
 
 // The calculator accepts input numbers from 1 to 10 (inclusive) and works exclusively with integers 
