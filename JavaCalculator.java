d5363365252f3f783094f4d95bd0ab2de590eb6d
Maksim Shesternev <shesternev.m@gmail.com>
2024-06-26 20:28:26 +0300

    new commit

--- JavaCalculator.java
+++ JavaCalculator.java
@@ -1,6 +1,10 @@
+import java.util.Scanner; //// все классы пакета util надо импортировать вручню, теперь можем получать данные на ввод
 public class JavaCalculator {
     public static void main(String[] args) {
-
+        Scanner scn = new Scanner(System.in); // инициализируем объект Scanner, который считывает данные из ввода в консоль
+        System.out.println("                 ***JavaCalculator***                   ");
+        System.out.println("введите свое выражение, которое надо посчитать (например 1+2):"); //по запуску программы отобразиться сперва этот текст
+        String input = scn.nextLine()
 
 
 
---
+++ out/production/JavaCalculator/README.md
@@ -0,0 +1,6 @@
+JavaCalculator
+
+My name is Maxim Shesternem, and I present my first project — a simple calculator written in Java. 
+This calculator supports basic arithmetic operations (addition, subtraction, multiplication and division)
+
+// The calculator accepts input numbers from 1 to 10 (inclusive) and works exclusively with integers 
